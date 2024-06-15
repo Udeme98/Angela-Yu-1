f9b86dc6594bea8b0235a9668b6a2eeb25987e19
Udeme98 <Darealudeme1@gmail.com>
2024-06-15 09:46:53 +0100

    JavaScript: Drum kit Project

--- JavaScript/Drum Kit Starting Files/index.js
+++ JavaScript/Drum Kit Starting Files/index.js
@@ -1,7 +1,50 @@
 let numberOfButtons = document.querySelectorAll("button").length;
+// console.log(numberOfButtons);
 
 for (let i = 0; i < numberOfButtons; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function () {
     // console.log("button " + i + " clicked");
+
+    const drum = this.innerHTML;
+    switch (drum) {
+      case "w":
+        let audioW = new Audio("sounds/tom-1.mp3");
+        audioW.play();
+        break;
+
+      case "a":
+        let audioA = new Audio("sounds/tom-2.mp3");
+        audioA.play();
+        break;
+
+      case "s":
+        let audioS = new Audio("sounds/tom-3.mp3");
+        audioS.play();
+        break;
+
+      case "d":
+        let audioD = new Audio("sounds/tom-4.mp3");
+        audioD.play();
+        break;
+
+      case "j":
+        let audioJ = new Audio("sounds/crash.mp3");
+        audioJ.play();
+        break;
+
+      case "k":
+        let audioK = new Audio("sounds/snare.mp3");
+        audioK.play();
+        break;
+
+      case "l":
+        let audioL = new Audio("sounds/kick-bass.mp3");
+        audioL.play();
+        break;
+
+      default:
+        console.log(drum);
+        break;
+    }
   });
 }
